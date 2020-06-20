2ab2ff05daa81faf2455a3f5d426220aedae7544
thiagoos16 <tos@icomp.ufam.edu.br>
2020-06-20 09:54:49 -0400

    add jwt middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     async register(req, res) {
@@ -13,7 +14,9 @@ module.exports = {
             
             user.password = undefined;
 
-            return res.send({ user });
+            const token = generateToken({ id: user.id });
+
+            return res.send({ user, token });
         } catch(err) {
             return res.status(400).send({error: 'Resgistration failed' });
         }
@@ -30,6 +33,16 @@ module.exports = {
         if (!await bcrypt.compare(password, user.password))
             return res.status(400).send({ error: 'Invalid password'});
         
-        res.send( { user } ); 
-    }
+        user.password = undefined;
+
+        const token =  generateToken({ id: user.id });
+
+        res.send( { user, token } ); 
+    },
+}
+
+function generateToken(params = {}) {
+    return jwt.sign({ params }, process.env.SECRET, {
+        expiresIn: 86400,
+    });
 }
