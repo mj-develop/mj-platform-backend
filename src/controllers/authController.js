b6015ce0e04bfa677fe248d992c0225edac85db0
thiagoos16 <tos@icomp.ufam.edu.br>
2020-06-20 08:58:40 -0400

    add validation email and password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,9 +2,16 @@ const User = require('../models/User');
 
 module.exports = {
     async register(req, res) {
+        const { email } = req.body;
+
         try {
+            if (await User.findOne({email}))
+                return res.status(400).send({ error: 'User already registraded'});
+
             const user = await User.create(req.body);
-    
+            
+            user.password = undefined;
+
             return res.send({ user });
         } catch(err) {
             return res.status(400).send({error: 'Resgistration failed' });
