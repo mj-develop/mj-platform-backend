63e8e862b3dc5310fc4f14c90f055f8c4ed8f5ff
thiagoos16 <tos@icomp.ufam.edu.br>
2020-07-26 19:03:52 -0400

    add auth middlw in me route

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const routes = express.Router();
+const authMiddleware = require('./middleware/auth');
 const AuthController = require('./controllers/authController');
 const StudentController = require('./controllers/studentController');
 
 routes.post("/register", AuthController.register);
 routes.post("/login", AuthController.login);
+routes.get("/profile", authMiddleware, AuthController.profile);
 routes.post("/forgot_password", AuthController.forgot_password);
 routes.post("/reset_password", AuthController.reset_password);
-
 
 routes.get("/students", StudentController.index);
 routes.get("/students/:id", StudentController.show);
