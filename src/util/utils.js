b3b8c47cae3027c943a120ca34a32e0a4906a6e6
thiagoos16 <tos@icomp.ufam.edu.br>
2020-08-15 20:16:12 -0400

    add utils functions to map errors

---
+++ src/util/utils.js
@@ -0,0 +1,40 @@
+const Course = require('../models/Course');
+const Student = require('../models/Student');
+
+module.exports = {
+    validateEmail(email) {
+        const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return re.test(email);
+    },
+
+    async validateCourseId(course_id) {
+        const course = await Course.findById(course_id);
+        return course;
+    },
+
+    async validateStudentId(student_id) {
+        const student = await Student.findById(student_id);
+        return student;
+    },
+
+    errors(err) {
+        const fields = Object.keys(err.errors).map((field_name) => {
+            return field_name; 
+        });
+    
+        const errors = fields.map((key) => {
+            if (key == "birth") 
+                return "birth.malformatted"
+    
+            if (key == "gender")
+                return "gender.can.be.m.or.f"
+
+            if (key == "course._id")
+                return "course.id.malformatted"  
+            
+            return err.errors[key] ? err.errors[key].properties.message : field_name; 
+        });
+        
+        return errors;
+    }
+}
